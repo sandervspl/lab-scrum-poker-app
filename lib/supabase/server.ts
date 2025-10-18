1fe6ee77d91e0ce3e3df56f901a25c633aa749c2
Sander Vispoel <sandervispoel@hotmail.com>
2025-10-18 12:35:52 +0200

    Use supabase-js createClient

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,21 +1,21 @@
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
-import { createServerClient } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js';
 
 import { Database } from './database.types';
 
 export function getSupabaseServerClient(cookieStore: ReadonlyRequestCookies) {
-  return createServerClient<Database>(
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
-        },
-      },
-    },
+    // {
+    //   cookies: {
+    //     getAll() {
+    //       return cookieStore.getAll();
+    //     },
+    //     setAll(cookiesToSet) {
+    //       cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+    //     },
+    //   },
+    // },
   );
 }
