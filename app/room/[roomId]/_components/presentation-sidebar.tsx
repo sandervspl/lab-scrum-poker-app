5d59ba088a97daf28ba9fef78b1bdcb80ca0ea7c
Sander Vispoel <sandervispoel@hotmail.com>
2025-10-20 21:52:23 +0200

    Replace with button

--- app/room/[roomId]/_components/presentation-sidebar.tsx
+++ app/room/[roomId]/_components/presentation-sidebar.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { useViewVotes } from '@/lib/hooks/use-view-votes';
-import { EyeIcon } from 'lucide-react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 
 import { PresentationModeToggle } from './presentation-mode-toggle';
 import { ResetVotesButton } from './reset-votes-button';
@@ -18,16 +19,17 @@ export function PresentationSidebar() {
         <CardHeader>
           <CardTitle className="text-lg">Controls</CardTitle>
         </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <div className="flex items-center justify-between">
-            <Label htmlFor="reveal-votes" className="text-sm">
-              <EyeIcon className="size-4" />
-              Show votes
-            </Label>
+        <CardContent className="flex flex-col gap-2">
+          {/* <div className="flex items-center justify-between">
+            <Label htmlFor="reveal-votes" className="text-sm"></Label>
             <Switch id="reveal-votes" checked={isRevealed} onCheckedChange={viewVotes} />
-          </div>
+          </div> */}
+          <Button size="sm" className="w-full" onClick={viewVotes}>
+            {isRevealed ? <EyeOffIcon className="size-4" /> : <EyeIcon className="size-4" />}
+            {isRevealed ? 'Hide votes' : 'Show votes'}
+          </Button>
           <ResetVotesButton className="w-full" />
-          <hr className="my-2" />
+          <hr className="my-4" />
           <PresentationModeToggle className="justify-between" />
         </CardContent>
       </Card>
