674c3513ccc4f5f9d8254869b732dbb011522201
Sander Vispoel <sandervispoel@hotmail.com>
2025-10-20 21:49:45 +0200

    Update presentation-sidebar.tsx

--- app/room/[roomId]/_components/presentation-sidebar.tsx
+++ app/room/[roomId]/_components/presentation-sidebar.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { useViewVotes } from '@/lib/hooks/use-view-votes';
-import { EyeIcon, RotateCcwIcon } from 'lucide-react';
+import { EyeIcon } from 'lucide-react';
 
 import { PresentationModeToggle } from './presentation-mode-toggle';
 import { ResetVotesButton } from './reset-votes-button';
